3cb1ad12b3e54061ba0c114698d6631d9335081f
Aapo <aapo.sirius.salminen9@gmail.com>
2022-12-05 08:54:55 +0200

    important

---
+++ flask/random_genre_request.py
@@ -0,0 +1,14 @@
+import requests
+
+request = "https://binaryjazz.us/wp-json/genrenator/v1/genre/"
+
+try:
+    response = requests.get(request)
+    if response.status_code == 200:
+        response_json = response.json()
+        print(response_json)
+    elif response.status_code == 404:
+        print(f"Hakua ei voitu suorittaa.\nVirhekoodi: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Hakua ei voitu suorittaa.\n{e}")
+
